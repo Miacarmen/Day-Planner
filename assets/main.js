291c96bbd5b22679a398e280ddb74e3ec6503412
Miacarmen <maitreyacarmen@gmail.com>
2022-04-07 23:32:47 -0700

    fixed Jquery bug in highlight function, removed undefined tr tag

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
 // function to display current day/time
-var date = $('.dateTime');
+var date = $(".dateTime");
 var getDate = function() {
 
     today = moment(new Date());
     // Day, Month, Date, Year, time
-    date.html(today.format('dddd, MMMM Do, YYYY, h:mm:ss a'));
+    date.html(today.format("dddd, MMMM Do, YYYY, h:mm:ss a"));
 };
 // setInterval for current time
 $(document).ready(function() {
@@ -15,38 +15,42 @@ $(document).ready(function() {
 
 // function to color code past, present, and future time blocks 
 var highlight = function() {
-    $('tbody tr').each(function(hour) {
+    $("tbody tr").each(function() {
         console.log($(this));
         // get current timeBlock
-        var timeBlock = $(this).attr('id').split('hour-')[1];
-        console.log(timeBlock);
+        var timeBlock = $(this).attr("id").split("hour-").at(1);
+        console.log($(this));
+        console.log($(this).attr('id'));
+        console.log($(this).attr('id').split("hour-"));
         // get the current Hour
         var dateTime = moment().hour();
         // if(timeBlock > dateTime)
         if (timeBlock < dateTime) {
             // turn those timeBlocks grey
-            $(this).addClass('past');
+            $(this).addClass("past");
+            $(this).removeClass("future present");
             // if(timeBlock = dateTime)
         } else if (timeBlock > dateTime) {
             // turn that block green
-            $(this).addClass('future');
+            $(this).addClass("future");
+            $(this).removeClass("past present");
             // if(timeBlock > dateTime)
         } else {
             // turn future time blocks pink
-            $(this).addClass('present');
-            // $(this).addClass('future');
+            $(this).addClass("present");
+            $(this).removeClass("future past");
         }
     });
 
-};
+}
 
 highlight();
 
 // function to enable type event/task onClick of add btn
-$('.btnAdd').on('click', function() {
+$(".btnAdd").on("click", function() {
     console.log($(this).parent().siblings(".event").children("textarea").val());
     var task = $(this).parent().siblings(".event").children("textarea").val();
-    var timeBlock = $(this).parent().siblings(".event").children("textarea").attr('id');
+    var timeBlock = $(this).parent().siblings('.event').children("textarea").attr("id");
     console.log(timeBlock);
     // save to local storage
     localStorage.setItem(timeBlock, task);
@@ -54,15 +58,14 @@ $('.btnAdd').on('click', function() {
 
 
 // function to store task on refresh
-$('textarea').each(function() {
-    console.log($(this).attr('id'));
-    $(this).val(localStorage.getItem($(this).attr('id')));
-})
-
-
+$("textarea").each(function() {
+    console.log($(this).attr("id"));
+    $(this).val(localStorage.getItem($(this).attr("id")));
+});
 
 
 // function for delete btn
-$('.btnDelete').click(function() {
+// $('.btnDelete').click(function() {
 
-});
+
+// });
