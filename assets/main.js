5ceb1fb50ed29369d651723effc8effb16c0f498
Miacarmen <maitreyacarmen@gmail.com>
2022-04-07 20:33:53 -0700

    added add btn and textarea functionality, added additional HTML tag classes, added past, present, future css classes

--- assets/main.js
+++ assets/main.js
@@ -14,19 +14,28 @@ $(document).ready(function() {
 });
 
 
-$('.content-container').addClass('background-color', blue);
-
 
 
 
 // function to color code current time and past time
 var highlight = function() {
-    // if(timeBlock > dateTime)
-    // turn those timeBlocks grey
+    $('tbody tr').each(function(hour) {
+            console.log($(this));
+            var timeBlock = $(this).attr('id').split('hour-')[1];
+            console.log(timeBlock);
+            var dateTime = moment().hour();
+            if (timeBlock < dateTime) {
+                $(this).addClass('past');
+            }
+
+        })
+        // if(timeBlock > dateTime)
+        // turn those timeBlocks grey
 
     // if time is now, highlight the appropriate timeBlock Green
 }
 
+highlight();
 
 // function to enable type event/task onClick
 var taskInput = $('.event');
@@ -38,13 +47,27 @@ var typeTask = function() {
 }
 
 
+$('.btnAdd').on('click', function() {
+    console.log($(this).parent().siblings(".event").children("textarea").val());
+    var task = $(this).parent().siblings(".event").children("textarea").val();
+    var timeBlock = $(this).parent().siblings(".event").children("textarea").attr('id');
+    console.log(timeBlock);
+
+    localStorage.setItem(timeBlock, task);
+});
+
+
+$('textarea').each(function() {
+    console.log($(this).attr('id'));
+    $(this).val(localStorage.getItem($(this).attr('id')));
+})
+
+
 
 // function for save btn
 var addTask = function() {
     // onClick it saves the event entered above in local storage
-    $('.btnAdd').click(function() {
 
-    })
 
     // if page is refreshed, the saved task is still shown
 }
